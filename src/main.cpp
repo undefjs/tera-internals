a59f12ba525737827edb16940927a9bbb346227d
undefjs <resetdungeon@gmail.com>
2018-10-13 13:19:32 +0200

    reorganize sdk

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <node.h>
+#include "sdk.h"
 
 namespace {
   using namespace v8;
   using namespace node;
 
   class TeraInternals {
+    TArray<UObjectEx*> *GObjects = NULL;
+    TArray<FNameEntry*> *GNames = NULL;
 
     TeraInternals() {
       return;
